8c0c7325de4bbb2d4cd7156d24b7b30f7681b6cc
Sami Lahtinen <thesamsai@gmail.com>
2018-10-19 21:31:31 +0300

    Continue running till infinity and beyond

--- Reflected_Light_Test.py
+++ Reflected_Light_Test.py
@@ -5,6 +5,8 @@ from ev3dev2.button import Button
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.motor import MoveTank
 
+import time
+
 sensors = ColorSensor()
 tank_drive = MoveTank("outA", "outD")
 
@@ -16,4 +18,5 @@ while True:
     else:
         tank_drive.off()
         tank_drive.on_for_rotations(100, -100, 0.8)
-         
+        tank_drive.on_for_rotations(50, 50, 200)
+        time.sleep(60)
