be3f52702c016266bb877e740918f96769f4a265
Sami Lahtinen <thesamsai@gmail.com>
2018-10-20 00:27:11 +0300

    Added abstractions for turning and moving forward

--- follow_line_algo.py
+++ follow_line_algo.py
@@ -8,7 +8,16 @@ from ev3dev2.motor import MoveTank
 import time
 
 while True:
-    follow_line(COLOR_BLACK)
+    follow_line(ColorSensor.COLOR_BLACK)
+
+def go_forward_slow(drive):
+    drive.on_for_rotations(20, 20, 0.5)
+
+def left_turn_45(drive):
+    drive.on_for_rotations(-50, 50, 1)
+
+def right_turn_45(drive):
+    drive.on_for_rotations(50, -50, 1)
 
 def follow_line(line_color):
     sensor = ColorSensor()
@@ -17,21 +26,22 @@ def follow_line(line_color):
     color = sensor.color()
 
     if color == line_color:
-        tank_drive.on_for_rotations(20, 20, 0.5)
+        go_forward(tank_drive)
     else:
         while color != line_color:
             tank_drive.off()
-            tank_drive.on_for_rotations(50, -50, 1)
+            left_turn_45(tank_drive)
+
             color = sensor.color()
 
             if color != line_color:
-                tank_drive.on_for_rotations(-50, 50, 1)
-                tank_drive.on_for_rotations(-50, 50, 1)
+                right_turn_45(tank_drive)
+                right_turn_45(tank_drive)
                 
                 color = sensor.color()
 
                 if color != line_color:
-                    tank_drive.on_for_rotations(-50, 50, 1)
+                    right_turn_45(tank_drive)
 
 
 
