e41b09f43541c3d010c26540476abd2a7f0aec66
Sami Lahtinen <thesamsai@gmail.com>
2018-10-20 14:23:15 +0300

    saatana

--- movement.py
+++ movement.py
@@ -8,47 +8,24 @@ tank_drive = MoveTank("outA", "outD")
 gyro = GyroSensor()
 
 def go_forward_slow():
-    drive.on_for_rotations(20, 20, 0.5)
+    tank_drive.on_for_rotations(20, 20, 0.5)
 
 def left_turn(angle):
     start_degrees = gyro.angle
     desired_degrees = start_degrees - angle
 
     while gyro.angle > desired_degrees:
-        drive.on_for_rotations(-50, 50, 1, block = False)
+        tank_drive.on_for_rotations(-50, 50, 1, block = False)
     
-    drive.off()
+    tank_drive.off()
 
 def left_turn(angle):
     start_degrees = gyro.angle
     desired_degrees = start_degrees + angle
 
     while gyro.angle < desired_degrees:
-        drive.on_for_rotations(50, -50, 1, block = False)
+        tank_drive.on_for_rotations(50, -50, 1, block = False)
     
-    drive.off()
-
-def follow_line(line_color):
-    sensor = ColorSensor()
-    
-    color = sensor.color
-
-    if color == line_color:
-        go_forward_slow(tank_drive)
-    else:
-        while color != line_color:
-            tank_drive.off()
-            left_turn_45(tank_drive)
-
-            color = sensor.color
-
-            if color != line_color:
-                right_turn_45(tank_drive)
-                right_turn_45(tank_drive)
-                
-                color = sensor.color
-
-                if color != line_color:
-                    right_turn_45(tank_drive)
+    tank_drive.off()
 
 
