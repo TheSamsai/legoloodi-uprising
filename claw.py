7ab849788ea0e3e9186ddd785a503dc621ca5ddf
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2018-10-20 05:45:53 +0300

    added claw

---
+++ claw.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import MediumMotor, OUTPUT_B
+
+
+def open():
+
+    motor = MediumMotor('outB')
+
+    motor.on_for_degrees(-100,800, block=False)
+
+
+
+def close():
+
+    motor = MediumMotor('outB')
+
+    motor.on_for_degrees(100,800, block=False)
