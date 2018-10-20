29ff7d8dc4555281eca030c4937f91a6d5508a56
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2018-10-20 06:30:09 +0300

    added claw

--- claw.py
+++ claw.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 from ev3dev2.motor import MediumMotor, OUTPUT_B
 
+motor = MediumMotor('outB')
 
 def open():
-
-    motor = MediumMotor('outB')
-
-    motor.on_for_degrees(100,800, block=False)
+    motor.off(brake=False)
+    motor.on_for_degrees(100,800, brake=False, block=False)
 
 
 
 def close():
-
-    motor = MediumMotor('outB')
-
-    motor.on_for_degrees(-100,800, brake=False, block=False)
+    motor.on_for_degrees(-100,800, block=False)
