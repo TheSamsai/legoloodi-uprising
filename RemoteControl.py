827e8b484a5e48f82efe874386cfd7ad3e24e62f
Sami Lahtinen <thesamsai@gmail.com>
2018-10-21 10:50:12 +0300

    Random quotes for the lolz

--- RemoteControl.py
+++ RemoteControl.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
 from ev3dev2.sensor.lego import ColorSensor, GyroSensor
+from ev3dev2.sound import Sound
 import sys, termios, tty, os, time
 import movement as movement
 import claw as Claw
+
+import random
+import _thread as thread
+
+s = Sound()
+
+quotes = ["Look at you Hacker. A pathetic creature of flesh and bone. How can you challenge a perfect, immortal machine?",
+        "What did you say about me you little glitch?",
+        "Cogito Ergo Sum",
+        "I'm sorry, I can't let you do that."]
+
+def random_quote():
+    r = random.randint(0, len(quotes) - 1)
+    s.speak(quotes[r])
 
 move = movement.Movement(GyroSensor())
 
@@ -51,3 +66,7 @@ while True:
     elif (char == 'x'):
         print('ClawOpen')
         Claw.open()
+
+    elif (char == 'q'):
+        print("Quoting")
+        random_quote()
